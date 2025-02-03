ae87af901a1446a1343c3e3e8203f4dbd51968fa
ErykNickel <eryk.nickel@elev.ga.lbs.se>
2025-02-03 10:30:08 +0100

    shooting

--- Profil spel/Assets/Scripts/PlayerShooting.cs
+++ Profil spel/Assets/Scripts/PlayerShooting.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float playerBulletSpeed = 10f;
+    [SerializeField] GameObject playerBullet;
+    [SerializeField] GameObject playerGun;
+    [SerializeField] float fireRate = 0.5f; // Cooldown duration
+    private float nextFireTime = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1")) // Assuming "Fire1" is mapped to shooting
+        {
+            TryFire();
+        }
+    }
+
+    void TryFire()
+    {
+        if (Time.time >= nextFireTime)
+        {
+            Fire();
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+
+    void Fire()
+    {
+        GameObject bullet = Instantiate(playerBullet, playerGun.transform.position, transform.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * playerBulletSpeed, ForceMode2D.Impulse);
     }
 }
