520d40fdc021e888b7ecf5d6b767df47f7034a89
ErykNickel <eryk.nickel@elev.ga.lbs.se>
2025-02-04 10:23:44 +0100

    Enemy prefab

--- Profil spel/Assets/Scripts/EnemyFollow.cs
+++ Profil spel/Assets/Scripts/EnemyFollow.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
+    [SerializeField] float followSpeed = 5f;
+    GameObject player;
+    
+
     // Start is called before the first frame update
     void Start()
     {
