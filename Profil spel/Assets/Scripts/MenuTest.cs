95faf42b7b3cb314399969cb525a5b9c1b1e23f3
Zeyrox51 <145931938+Zeyrox51@users.noreply.github.com>
2025-02-05 14:40:54 +0100

    fixat menu settings
    har fixat menu settings panel men jag måste overlay rätt på menu

---
+++ Profil spel/Assets/Scripts/MenuTest.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MenuTest : MonoBehaviour
+{
+    VisualElement main = null;
+    VisualElement settings = null;
+
+    VisualElement root;
+
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        root = GetComponent<UIDocument>().rootVisualElement;
+    }
+
+    void Start()
+    {
+        Button settingsButton = root.Q<Button>("Settings_button");
+        Button mainButton = root.Q<Button>("Exit");
+
+        settings = root.Q<VisualElement>("SettingsPanel");
+        main = root.Q<VisualElement>("Exit");
+
+        mainButton.RegisterCallback<ClickEvent>(x => settings.style.display = DisplayStyle.None);
+        settingsButton.RegisterCallback<ClickEvent>(x => settings.style.display = DisplayStyle.Flex);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
